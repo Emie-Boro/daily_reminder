4f0e2aba9c3927f256469446241ae89c3ef3655b
Emie Boro <emiedonmukumo@gmail.com>
2023-06-03 14:35:28 +0100

    Added bootstrap

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 const colorList = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 
-const namePrompt = prompt('What is your name?')
-document.getElementById('name').innerHTML = namePrompt
-document.title = namePrompt
 const htmlTime = document.getElementById('time');
 
 
 setInterval(()=>{
-    const righthexColor1 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-    const righthexColor2 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-    const righthexColor3 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
+    const hexColor1 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
+    const hexColor2 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
+    const hexColor3 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
 
-    const lefthexColor1 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-    const lefthexColor2 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-    const lefthexColor3 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
+    const hexColor = `#${hexColor1}${hexColor2}${hexColor3}`;
 
-    const lefthexColor = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-
-    const rightColor = `#${righthexColor1}${righthexColor2}${righthexColor3}`;
-    const leftColor = `#${lefthexColor1}${lefthexColor2}${lefthexColor3}`;
-    document.body.style.backgroundImage = `linear-gradient(to bottom right, ${rightColor}, ${leftColor})`
-    document.body.style.backgroundSize = 'cover'
-    document.body.style.backgroundPosition = 'center'
-    document.body.style.backgroundRepeat = 'no-repeat'
-    document.getElementById('color').innerHTML = `${rightColor}  ${leftColor}`
-
-}, 500)
+    document.body.style.backgroundColor = hexColor;
+    document.getElementById('color').innerHTML = hexColor;
+}, 60000)
 
 setInterval(()=>{
-    document.getElementById('time').innerHTML = new Date()
+    let ampm= null;
+    if(new Date().getHours >= '12') {
+        ampm = 'Good Afternoon'
+    } else{
+        ampm = 'Good Morning'
+    }
+    document.getElementById('greet').innerHTML = ampm;
+    const time = `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`;
+    document.getElementById('time').innerHTML = time;
 }, 1000)
