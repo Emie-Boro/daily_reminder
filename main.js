a7ff52d81b012976b170a51d6eda3c1b16824f3a
Emie Boro <emiedonmukumo@gmail.com>
2023-06-03 16:01:50 +0100

    Daily reminder

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-const colorList = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+const quoteList = [
+        'Life comes from the earth and life returns to the earth',
+        "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart",
+        'Be brave enough to live the life of your dreams according to your vision and purpose instead of the expectations and opinions of others',
+        'Do what is right, not what is easy nor what is popular',
 
-const htmlTime = document.getElementById('time');
+    ]
 
-
-setInterval(()=>{
-    const hexColor1 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-    const hexColor2 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-    const hexColor3 = `${colorList[Math.floor(Math.random() * colorList.length)]}${colorList[Math.floor(Math.random() * colorList.length)]}`;
-
-    const hexColor = `#${hexColor1}${hexColor2}${hexColor3}`;
-
-    document.body.style.backgroundColor = hexColor;
-    document.getElementById('color').innerHTML = hexColor;
-}, 60000)
+    setInterval(()=>{
+        document.getElementById('quote').innerHTML = quoteList[Math.floor(Math.random() * quoteList.length)];
+    }, 60000)
 
 setInterval(()=>{
-    let ampm= null;
+    let ampm = null;
     if(new Date().getHours >= '12') {
         ampm = 'Good Afternoon'
     } else{
         ampm = 'Good Morning'
     }
-    document.getElementById('greet').innerHTML = ampm;
+    const greet = document.getElementById('greet')
+    greet.innerHTML = ampm;
+
     const time = `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`;
     document.getElementById('time').innerHTML = time;
-}, 1000)
+})
